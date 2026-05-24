342e3f4cf6caa43cdbf2d6330964656c30e046d6
pierrebm12 <infodevtro@gmail.com>
2026-05-24 18:43:56 -0500

    Add request logging to debug 502

--- server/index.js
+++ server/index.js
@@ -33,6 +33,12 @@ app.use(cors({
 
 app.use(express.json({ limit: "1mb" }));
 
+// Request logging middleware
+app.use((req, res, next) => {
+  console.error(`📥 ${req.method} ${req.url}`);
+  next();
+});
+
 const apiLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 100,
